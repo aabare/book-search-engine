ea0505a0732a846c1b0556e4f360794a9dbac2a5
Adam <a.6942@yahoo.com>
2022-01-30 22:32:06 -0500

    fixed connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://aabare:password1234@cluster0.s8l4h.mongodb.net/test`, {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb+srv://aabare:password1234@cluster0.s8l4h.mongodb.net/test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
